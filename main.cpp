84bae75e98455c4e3e8c0feaba7dc5b4825fba6f
ArthoPacini <artho_pacini@icloud.com>
2019-04-08 16:02:32 -0300

    Update main.cpp
    Separate include files and included sample files

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 #include <filesystem>
 #include <string>
 
-#include "binaryio.h" //This is the binary input / output library, for writing and reading binary files with ease, also encoding
+#include "binaryio.h" //This is the binary input / output library, for writing and reading binary files with ease
+#include "encode.h" //This is the library to encode ppm files
+#include "decode.h" //This is the library to decode ppm files
 
 int main()
 {
     //Sets the path for input file
     fs::path inputPath = fs::current_path();
-    inputPath /= "input.ppm";
+    inputPath /= "samples";
+    inputPath /= "beach.ppm";
 
     //Sets the path for the output file
     fs::path outputPath = fs::current_path();
-    outputPath /= "output.ppm";
+    outputPath /= "output_images";
+    outputPath /= "beach_output.ppm";
     
     //This will encode the image with the message
     encodeImage(inputPath, outputPath, "This is a hidden message inside the file! ");
